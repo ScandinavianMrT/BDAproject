6acd6306aaf803ef73ce2c86c446d82a7fc67de0
ScandinavianMrT <troelspmjensen@gmail.com>
2021-10-25 10:32:30 +0200

    imputations

--- EDA.R
+++ EDA.R
@@ -1,7 +1,6 @@
 library(readr)
 library(dplyr)
 library(caret)
-library(amelia)
 
 #Load datasets
 life <- read_csv("data/life_expectancy.csv")
@@ -15,17 +14,25 @@ life$Status <- ifelse(life$Status == "Developed", 1,0)
 #As life expecancy is our dependend variable and only 10 observations are missing, we will simply drop those
 life <- life[complete.cases(life_stand$`Life expectancy`), ]
 
-#Alcohol has many NaN values, so we fill NaN with average value from observations from same country
-#for (obs in life$Alcohol) {
- # if(is.na(obs) == TRUE) {
-  #  obs <- mean(life$Alcohol[life$Alcohol == life$Country[obs])}
-  #}
-
-life$Alcohol[is.na(life$Alcohol)] <- mean(life$Alcohol[life$Alcohol == life$Country))
+new_life <- life %>%  group_by(Country) %>%
+  mutate_all(funs(ifelse(is.na(.), mean(., na.rm = TRUE),.)))
 
 #Standardize data to have unit variance and zero mean as data variables are 
 # given in incomensurable units and regression model will rely on distance measure
 life_preproc <- preProcess(select(life, 1, 4:22), method=c("center", "scale"))
 life_stand <- predict(life_preproc, select(life, 1, 3:22))
 
+
+
+#for(i in 1:ncol(life)){
+# life %>% group_by("Country") 
+#life[is.na(life[,i]), i] <- mean(data[,i], na.rm = TRUE)
+#}
+
+#Alcohol has many NaN values, so we fill NaN with average value from observations from same country
+#for (colname in colnames(life)) {
+ # test <- life %>% 
+  #  group_by(Country) %>% 
+   # mutate(life$colname) <- ifelse(is.na(Alcohol), mean(Alcohol,na.rm=TRUE), Alcohol))
+#}
 
