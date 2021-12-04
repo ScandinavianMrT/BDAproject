8605cc11dc612f0ac80f6f64d9b95e405134e651
ScandinavianMrT <troelspmjensen@gmail.com>
2021-12-04 12:30:25 +0100

    small changes

--- EDA.R
+++ EDA.R
@@ -12,11 +12,11 @@ library(imputeTS)
 library(data.table)
 
 #Load datasets
-life <- read_csv("data/life_expectancy.csv")
+life <- read_csv("data/Life_Expectancy_Data_2.0.csv")
 env_var <- read_csv("data/environmental_variables.csv")
 emission <- select(read_csv("data/emission_data.csv"), 1, 251:266) # delete all columns except country and 2000:2015
 military <- select(read_csv("data/military_expenditure.csv"), 1, 45:60) # delete all columns except country and 2000:2015
-HappinessMean <- select(read_csv("data/Hapmean.csv"),2,9)
+HappinessMean <- select(read_csv("data/happiness.csv"),1,8)
 
 #Convert emission and military data to long format for merging, renaming some variables
 emission_long <- melt(setDT(emission), id.vars = "Country")[order(Country, -variable),]
@@ -26,19 +26,20 @@ military_long <- melt(setDT(military), id.vars = "Name")[order(Name, -variable),
 names(military_long)[names(military_long) == 'Name'] <- 'Country'
 names(military_long)[names(military_long) == 'variable'] <- 'Year'
 names(military_long)[names(military_long) == 'value'] <- 'Military Expenditure'
-names(HappinessMean)[2] <- "Happiness Mean Score"
+names(HappinessMean)[names(HappinessMean) == 'Mean'] <- 'Mean happiness'
+
 
 #Merge military, emission and happiness dataset with life dataset
 life_emiss <- merge(life, emission_long, by = c("Country", "Year"))
 df1 <- merge(life_emiss, military_long, by = c("Country", "Year"))
-df <- merge (df1,HappinessMean, by=c("Country"), all.x = TRUE)
+
 
 #Save descriptive statistics as dataframe using psych package - #HOW TO GET CORRECT VALUES???
-df_summary <- as.data.frame(describe(df), digits=2)
+df_summary <- as.data.frame(describe(df1), digits=2)
 
 #Impute NA's using mean of respective countries in places where some values are present for that country
 missmap(df)
-df_imp1 <- df %>%  group_by(Country) %>%
+df_imp1 <- df1 %>%  group_by(Country) %>%
   mutate_all(funs(ifelse(is.na(.), mean(., na.rm = TRUE),.)))
 
 #Impute remaining NA values by mean of column using imputeTS package
@@ -86,15 +87,26 @@ ggqqplot(df_imp2$Alcohol,main="Distribution of Alcohol Consumption",
   scale_y_continuous(expand = c(0, 0), limits = c(0, NA))
 
 #Standardize data to have unit variance and zero mean as data variables are 
-# given in incomensurable units and regression model will rely on distance measure
+#given in incomensurable units
 df_stand <- df_imp2
 df_stand[4:24] <- scale(df_stand[4:24], center = TRUE, scale = TRUE)
 
 #Correlation table and plot
-df_cor <- cor(df_imp2[4:25])
+df_cor <- cor(df_imp2[4:24])
 cor_matrix <- as.data.frame(df_cor)
 corrplot(df_cor, method = 'color')
 
 #Export datatable
-write.csv(df_stand,"FinalDf.csv", row.names = FALSE)
+write.csv(df_stand,"data/FinalDf.csv", row.names = FALSE)
+
+#Create new dataset with years aggregated and happiness added
+df_new <- aggregate(df_stand[, 3:22], list(df_stand$Country), mean)
+names(df_new)[names(df_new) == 'Group.1'] <- 'Country'
+df_new <- merge(df_new,HappinessMean, by=c("Country"), all.x = TRUE)
+
+#Impute missing values of happiness data by mean of column
+df_new$`Mean happiness` <- na_mean(df_new$`Mean happiness`)
+
+#Export datatable
+write.csv(df_new,"data/aggregated_data.csv", row.names = FALSE)
 
