a6dad082ee72a5c0d030ab6b15a277d823f90050
ScandinavianMrT <troelspmjensen@gmail.com>
2021-12-04 12:31:18 +0100

    lm model added

--- model_fit_v2.R
+++ model_fit_v2.R
@@ -7,7 +7,7 @@ library(plm)
 library(janitor)
 library(stargazer)
 
-data <- read_csv("data/data_proc.csv")
+data <- read_csv("data/FinalDF.csv")
 df <- data %>%
   janitor::clean_names()
 
@@ -16,54 +16,68 @@ attach(df)
 Y <- cbind(life_expectancy)
 X <- cbind(status,adult_mortality,infant_deaths,alcohol,percentage_expenditure,
            hepatitis_b,measles,bmi,under_five_deaths,polio,total_expenditure,
-           diphtheria,hiv_aids,gdp,population,thinness_1_19_years,
            thinness_5_9_years,income_composition_of_resources,schooling,
            emission,military_expenditure)
+
+#Delete obviously correlated variables
+X2 <- cbind(status,alcohol,percentage_expenditure,
+            hepatitis_b,measles,bmi,polio,total_expenditure,
+            diphtheria,hiv_aids,gdp,population,thinness_1_19_years,
+            thinness_5_9_years,income_composition_of_resources,schooling,
+            emission,military_expenditure)
+
 pdf <- pdata.frame(df, index = c("country", "year"))
 
 #Fit ols model and inspect residuals
 ols <- plm(Y ~ X, data = pdf, model = "pooling")
 sum_ols <- stargazer(ols, type="text")
-print(sum_ols)
-ols_residuals = residuals(ols)
-ols_fit = fitted(ols)
+#Omit correlated variables
+ols2 <- plm(Y ~ X2, data = pdf, model = "pooling")
+sum_ols2 <- stargazer(ols2, type="text")
+#R2 goes up quite a bit
 
 #Fit between model
 between <- plm(Y ~ X, data = pdf, model = "between")
 sum_between <- stargazer(between, type="text")
-print(sum_between)
+#Omit correlated variables
+between2 <- plm(Y ~ X2, data = pdf, model = "between")
+sum_between2 <- stargazer(between2, type="text")
+#R2 goes down
 
-#Fit first difference model
-fdiff <- plm(Y ~ X, data = pdf, model = "fd")
-sum_fdiff <- stargazer(fdiff, type="text")
-print(sum_fdiff)
-
-#fit fixed effect model - Subtracts mean of group for each gruop to isolate variations within group
+#fit fixed effect model - Subtracts mean of group for each group to isolate variations within group
 fixed <- plm(Y ~ X, data = pdf, model = "within")
 sum_fixed <- stargazer(fixed, type="text")
-print(sum_fixed)
+#Omit correlated variables
+fixed2 <- plm(Y ~ X2, data = pdf, model = "within")
+sum_fixed2 <- stargazer(fixed2, type="text")
+#R2 goes up
 
 #fit random effect model
 random <- plm(Y ~ X, data = pdf, model = "random")
-sum_rand <- stargazer(random, type="text")
-print(sum_rand)
+sum_random <- stargazer(random, type="text")
+#Omit correlated variables
+random2 <- plm(Y ~ X2, data = pdf, model = "random")
+sum_random2 <- stargazer(random2, type="text")
+#R2 goes up
+
+#extract fixed effects from fixed model
+fixef(fixed)
 
 #Breush Pagan Test - null hypothesis that the variance of the residuals is constant
 lmtest::bptest(fixed)
 #p-value less than 0.05 so we can reject null and conclude heteroscedacity is present
-#ols does not work with heteroscedacity #https://www.r-bloggers.com/2016/01/how-to-detect-heteroscedasticity-and-rectify-it/
+#OLS does not work with heteroscedacity #https://www.r-bloggers.com/2016/01/how-to-detect-heteroscedasticity-and-rectify-it/
 
 #Test which model is better
 pFtest(fixed, ols)
 phtest(fixed, random)
-#Fixed is better than random as null rejected - However, r2 higher with RE
-
-#extract fixed effets from model
-fixef(fixed)
+phtest(fixed, between)
+#Fixed is better than random and between as null rejected - However, r2 higher with RE and between
 
 #Check to see if Panel Effects exist in data
 plmtest(ols, type=c("bp"))
 #p-value is than 0.05 - we can reject null and conclude that panel effects exist in data
+#Therefore should not use OLS as it disregards panel effects
 
 #Test for cross-sectional dependence
 pcdtest(fixed, test="lm")
@@ -72,10 +86,8 @@ pcdtest(fixed, test="cd")
 
 #Breusch-Godfrey/Wooldridge test for serial correlation in panel models
 pbgtest(fixed)
+#Null rejected - serial correlation in idiosyncratic errors
 
 #Durbin-Watson test for serial correlation in panel models
 pdwtest(fixed)
 #null rejected - residuals are autocorrelated
-
-fix_fit <- fitted(fixed)
-predict(fit)
